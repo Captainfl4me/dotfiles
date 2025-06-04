579e72e867eb4ae45ee70a1f856664de7ca7ae8c
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-06-04 21:04:53 +0200

    remove additionnal galaxy_book driver

--- install_scripts/SAMSUNG-Arch-Nico.sh
+++ install_scripts/SAMSUNG-Arch-Nico.sh
@@ -15,23 +15,6 @@ else
 	cd ${SCRIPT_DIR}
 fi
 
-if [[ ! -d ~/aur/samsung-galaxybook-extras ]]
-then
-	echo "${NOTE} Install Samsung driver.........." | tee -a "$LOG"
-	cd ~/aur
-	git clone https://github.com/joshuagrisham/samsung-galaxybook-extras.git || { printf "%s - Failed to clone ${YELLOW}samsung-galaxybook-extras${RESET}\n" "${ERROR}"; exit 1; }
-	sudo dkms add ~/aur/samsung-galaxybook-extras
-	cd ${SCRIPT_DIR}
-else
-	echo "${NOTE} Update Samsung driver.........." | tee -a "$LOG"
-	cd ~/aur/samsung-galaxybook-extras
-	git pull
-	cd ${SCRIPT_DIR}
-fi
-sudo dkms build samsung-galaxybook/extras
-sudo dkms install samsung-galaxybook/extras
-sudo mkinitcpio -P
-
 echo -e "${NOTE} Writing ${YELLOW}necessary-verbs${RESET} crontab..."
 echo -e "0 * * * * env PATH=$PATH DISPLAY=$DISPLAY DESKTOP_SESSION=$DESKTOP_SESSION DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS /usr/bin/zsh /home/nicoth/aur/galaxy-book2-pro-linux/sound/necessary-verbs.sh" | sudo crontab -
 
